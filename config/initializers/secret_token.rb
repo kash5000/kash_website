320a346605b85ca76acd0cd81210a3292edcecbe
Kashyap Iyengar <kash5000@gmail.com>
2012-04-14 19:46:20 -0400

    My Website Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KashWebsite::Application.config.secret_token = '46955938f1586ad66c663c323dbd2ef9fbbffd4abaf053aac9bee593939f6daa8a354ad071c365b3b7ed0cc7586dad788e624207a6e93df05dde418cc2c62514'
